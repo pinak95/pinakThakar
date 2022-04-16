178818e76dd9e0c5f280d9c31d052be727b7d93a
Redsuit-Deadpool <pinak.thakar95@gmail.com>
2022-04-16 16:05:20 +0530

    An attempt to not redirect

--- script.js
+++ script.js
@@ -54,7 +54,44 @@ function setTheme(mode){
 	localStorage.setItem('theme', mode)
 }
 
-
+window.addEventListener("DOMContentLoaded", function () {
+  
+	var form = document.getElementById("contact-form");
+	
+	var status = document.getElementById("status");
+  
+	function success() {
+	  form.reset();
+	  status.classList.add("success");
+	  status.innerHTML = "Thanks!";
+	}
+  
+	function error() {
+	  status.classList.add("error");
+	  status.innerHTML = "Error!";
+	}
+  
+	form.addEventListener("submit", function (ev) {
+	  ev.preventDefault();
+	  var data = new FormData(form);
+	  ajax(form.method, form.action, data, success, error);
+	});
+  });
+  
+  function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+	  if (xhr.readyState !== XMLHttpRequest.DONE) return;
+	  if (xhr.status === 200) {
+		success(xhr.response, xhr.responseType);
+	  } else {
+		error(xhr.status, xhr.response, xhr.responseType);
+	  }
+	};
+	xhr.send(data);
+  }
   
 
 
